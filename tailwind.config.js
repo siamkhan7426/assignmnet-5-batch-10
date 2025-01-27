8f1e20460b0fb798a8f90198cb12ef719818cc34
siamkhan7426 <sieam.khan12@gmail.com>
2025-01-27 12:01:46 +0600

    naver scetion hero section blog html

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    theme: {
+      extend: {
+        colors: {
+          primar: '#B4F461', // কাস্টম কালার (যেমন: Primary)
+          secondary: {
+            light: '#34d399',
+            DEFAULT: '#10b981', // Default কালার
+            dark: '#059669',
+          },
+        },
+      },
+    },
+    plugins: [],
+  };
+  
