6de87d1b8ffd418e548544ede1576e083ccec496
siamkhan7426 <sieam.khan12@gmail.com>
2025-01-29 19:53:53 +0600

    data theme add and function add new

--- js/index.js
+++ js/index.js
@@ -4,10 +4,12 @@ function getInputValue (id){
     return parseFloat(document.getElementById(id).value);
 };
 
-
 // coin function 
 function DonateCoinvalue (id){
     return parseFloat(document.getElementById(id).innerText);
+};
+function hInnerText (id){
+    return document.getElementById(id).innerText;
 };
 
 
@@ -45,16 +47,20 @@ document.getElementById("modal-close").addEventListener("click", function () {
     hideModal("doantion-modal");
 });
 
+
 // function vitor 4ta parametter naw hoicha..! code not repit 
-function setupDonation(buttonId, inputId, donateCoinId, balanceId, modalId) {
+function setupDonation(buttonId, inputId, donateCoinId, balanceId, modalId, innerHt) {
     const button = document.querySelector(buttonId);
     button.addEventListener("click", function () {
         const inputValue = getInputValue(inputId);
         const donateCoin = DonateCoinvalue(donateCoinId);
         const totalBalance = DonateCoinvalue(balanceId);
+        const hHtml = hInnerText(innerHt);
+        console.log(hHtml)
+        
 
         if (inputValue <= 0 || isNaN(inputValue)) {
-            alert("Enter a valid number!");
+            alert("invalid Donation Amount...!");
             return;
         }
 
@@ -66,29 +72,26 @@ function setupDonation(buttonId, inputId, donateCoinId, balanceId, modalId) {
         setElementValue(balanceId, totalBalance - inputValue);
         setElementValue(donateCoinId, donateCoin + inputValue);
         showModal(modalId);
-        
         const historyDiv = document.createElement("div");
         historyDiv.innerHTML = `
           <div class="grid m-3 w-10/12  space-y-3 border border-solid border-gray-400 py-2 px-1 rounded-lg">
-            <h4 class="text-xl font-bold">${inputValue} Taka is Donated for famine-2024 at Feni, Bangladesh</h4>
+            <h4 class="text-xl font-bold">${inputValue} Taka is ${hHtml}</h4>
             <p class="text-lg text-gray-400">Date: ${new Date()}</p>
             </div>
           `
           const histroyCardSection = document.querySelector("#history-card-section");
-          console.log(histroyCardSection)
           histroyCardSection.insertBefore(historyDiv, histroyCardSection.firstChild);
          
     });
    
 }
 
-
-
 // alda card jonno function agrumnet pass 
-setupDonation("#noakhali-btn", "noakhali-input", "donate-nukhali","donate-balance", "doantion-modal",);
-setupDonation("#btn-feni", "feni-input", "donate-feni","donate-balance", "doantion-modal");
-setupDonation("#quota-btn", "quota-input", "donate-quota","donate-balance", "doantion-modal");
+setupDonation("#noakhali-btn", "noakhali-input", "donate-nukhali","donate-balance", "doantion-modal", "n-title");
+setupDonation("#btn-feni", "feni-input", "donate-feni","donate-balance", "doantion-modal", "f-title");
+setupDonation("#quota-btn", "quota-input", "donate-quota","donate-balance", "doantion-modal", "q-title");
 
+// toogle button and history card 
 const historyBtn = document.querySelector("#history-button");
 const donationBtn = document.querySelector("#donation-button");
 historyBtn.addEventListener("click", function(){
@@ -101,30 +104,10 @@ donationBtn.addEventListener("click", function(){
     donationBtn.classList.add("border-none", "bg-[#B4F461]");
     historyBtn.classList.remove("border-none", "bg-[#B4F461]");
     document.querySelector("#card-section").classList.remove("hidden")
+    document.querySelector("#history-card-section").classList.add("hidden");
 })
 
 
-
-
-
-
-// const historyTab = document.querySelector("#donation-button");
-// // const donateTab = document.querySelector("#history-button");
-// // const cardSection = document.querySelector("#card-section");
-// // const histroyCardSection = document.querySelector("#history-card-section");
-// historyTab.addEventListener("click", function(){
-//     historyTab.classList.add("border-none", "bg-[#B4F461]");
-   
-
-// })
-// // toggle button
-//  // donateTab.classList.remove("border-none", "bg-[#B4F461]");
-//     // cardSection.classList.remove("hidden")
-//     // histroyCardSection.classList.remove("hidden");
-//     // donateTab.classList.add("border-none", "bg-[#B4F461]");
-//     // historyTab.classList.remove("border-none", "bg-[#B4F461]");
-//     // cardSection.classList.add("hidden");
-//     // histroyCardSection.classList.add("hidden");
     
 
 
